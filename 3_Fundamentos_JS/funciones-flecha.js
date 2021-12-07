1ce3ea19875ec9d352bd17e5069e10601c352163
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-12-06 19:08:00 -0500

    Arrow Functions

---
+++ 3_Fundamentos_JS/funciones-flecha.js
@@ -0,0 +1,15 @@
+function sumar(a, b = 10) {
+    return a + b
+}
+console.log(sumar(5))
+
+
+const restar = (a, b = 5) => {
+    return a - b
+}
+console.log(restar(10))
+
+
+const mult = (a, b) => a * b
+console.log(mult(5, 10))
+
