0bf98790a45c6ee7aa8062a6164708d45d201072
Carlos David Páez Ferreira <carlos.paezf@usantoto.edu.co>
2021-12-17 17:54:26 -0500

    RESTServer - Corrección del controlador de Search

--- 13_Archivos/controllers/search.controller.js
+++ 13_Archivos/controllers/search.controller.js
@@ -13,118 +13,143 @@ const allowedCollections = [
 ]
 
 
-const searchUsers = async (term = '', res = response) => {
-    try {
-        const isMongoId = ObjectId.isValid(term)
-        if (isMongoId) {
-            const user = await User.findById(term)
-            return res.json({
-                results: user ? [user] : []
-            })
+const searchUsers = (term = '') => {
+    let res
+    return new Promise(async (resolve, reject) => {
+        try {
+            const isMongoId = ObjectId.isValid(term)
+            if (isMongoId) {
+                const user = await User.findById(term)
+                res = {
+                    results: user ? [user] : []
+                }
+                return resolve(res)
+            }
+
+            const regex = new RegExp(term, 'i')
+
+            const [total, users] = await Promise.all([
+                User.count({
+                    $or: [{ name: regex }, { email: regex }],
+                    $and: [{ status: true }]
+                }),
+                User.find({
+                    $or: [{ name: regex }, { email: regex }],
+                    $and: [{ status: true }]
+                })
+            ])
+
+            res = {
+                total,
+                results: users
+            }
+
+            return resolve(res)
+        } catch (error) {
+            console.log(`Error: `.red)
+            return reject({ error, status: 500 })
         }
-
-        const regex = new RegExp(term, 'i')
-
-        const [total, users] = await Promise.all([
-            User.count({
-                $or: [{ name: regex }, { email: regex }],
-                $and: [{ status: true }]
-            }),
-            User.find({
-                $or: [{ name: regex }, { email: regex }],
-                $and: [{ status: true }]
-            })
-        ])
-
-        return res.json({
-            total,
-            results: users
-        })
-    } catch (error) {
-        console.log(`Error: `.red)
-        throw new Error(error)
-    }
+    })
 }
 
 
-const searchCategories = async (term = '', res = response) => {
-    try {
-        const isMongoId = ObjectId.isValid(term)
-        if (isMongoId) {
-            const category = await Category.findById(term).populate({
-                path: 'user',
-                select: 'name email'
-            })
-            return res.json({
-                results: category ? [category] : []
-            })
+const searchCategories = (term = '') => {
+    let res
+    return new Promise(async (resolve, reject) => {
+        try {
+            const isMongoId = ObjectId.isValid(term)
+            if (isMongoId) {
+                const category = await Category.findById(term).populate({
+                    path: 'user',
+                    select: 'name email'
+                })
+                res = {
+                    results: category ? [category] : []
+                }
+                return resolve(res)
+            }
+
+            const regex = new RegExp(term, 'i')
+
+            const [total, categories] = await Promise.all([
+                Category.count({ name: regex, status: true }),
+                Category.find({ name: regex, status: true }).populate({
+                    path: 'user',
+                    select: 'name email'
+                })
+            ])
+
+            res = {
+                total,
+                results: categories
+            }
+
+            return resolve(res)
+        } catch (error) {
+            console.log(`Error: `.red)
+            return reject({ error, status: 500 })
         }
+    })
 
-        const regex = new RegExp(term, 'i')
-
-        const [total, categories] = await Promise.all([
-            Category.count({ name: regex, status: true }),
-            Category.find({ name: regex, status: true }).populate({
-                path: 'user',
-                select: 'name email'
-            })
-        ])
-
-        return res.json({
-            total,
-            results: categories
-        })
-    } catch (error) {
-        console.log(`Error: `.red)
-        throw new Error(error)
-    }
 }
 
 
-const searchProducts = async (term = '', res = response) => {
-    try {
-        const isMongoId = ObjectId.isValid(term)
-        if (isMongoId) {
-            const product = await Product.findById(term).populate('user', 'name').populate('category', 'name')
-            return res.json({
-                results: product ? [product] : []
-            })
+const searchProducts = (term = '') => {
+    let res
+    return new Promise(async (resolve, reject) => {
+        try {
+            const isMongoId = ObjectId.isValid(term)
+            if (isMongoId) {
+                const product = await Product.findById(term)
+                    .populate('user', 'name')
+                    .populate('category', 'name')
+                res = {
+                    results: product ? [product] : []
+                }
+                return resolve(res)
+            }
+
+            const regex = new RegExp(term, 'i')
+
+            const [total, products] = await Promise.all([
+                Product.count({ name: regex, status: true }),
+                Product.find({ name: regex, status: true })
+                    .populate('user', 'name')
+                    .populate('category', 'name')
+            ])
+
+            res = {
+                total,
+                results: products
+            }
+            return resolve(res)
+        } catch (error) {
+            console.log(`Error: `.red)
+            return reject({ error, status: 500 })
         }
-
-        const regex = new RegExp(term, 'i')
-
-        const [total, products] = await Promise.all([
-            Product.count({ name: regex, status: true }),
-            Product.find({ name: regex, status: true }).populate('user', 'name').populate('category', 'name')
-        ])
-
-        return res.json({
-            total,
-            results: products
-        })
-    } catch (error) {
-        console.log(`Error: `.red)
-        throw new Error(error)
-    }
+    })
 }
 
 
-const search = (req, res = response) => {
+const search = async (req, res = response) => {
     const { collection, term } = req.params
 
     if (!allowedCollections.includes(collection)) return res.status(400).json({ msg: `Las colecciones permitidas son ${allowedCollections}` })
 
-    switch (collection) {
-        case 'users': searchUsers(term, res); break;
-        case 'categories': searchCategories(term, res); break;
-        case 'products': searchProducts(term, res); break;
-        default: res.status(500).json({ msg: 'Búsqueda no controlada' }); break;
+    let results
+
+    try {
+        switch (collection) {
+            case 'users': results = await searchUsers(term); break;
+            case 'categories': results = await searchCategories(term); break;
+            case 'products': results = await searchProducts(term); break;
+            default: res.status(500).json({ msg: 'Búsqueda no controlada' }); break;
+        }
+        return res.json(results)
+    } catch ({ error, status }) {
+        console.log(error)
+        res.status(status).json(error)
     }
-    res.json({
-        msg: 'Buscar',
-        collection,
-        term
-    })
 }
 
 
